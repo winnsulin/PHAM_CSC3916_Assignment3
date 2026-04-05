684087a83087fb605e6d8117b1fd4e92f4499e5b
winnsulin <wpham213@gmail.com>
2026-04-05 17:10:57 -0600

    created Reviews.js

---
+++ Reviews.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect(process.env.DB);
+
+// Review schema
+var ReviewSchema = new Schema({
+  movieId: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'Movie', 
+    required: true 
+  },
+  username: { 
+    type: String, 
+    required: true, 
+    trim: true 
+  },
+  review: { 
+    type: String, 
+    required: true 
+  },
+  rating: { 
+    type: Number, 
+    required: true, 
+    min: 0, 
+    max: 5 
+  }
+});
+
+// return the model
+module.exports = mongoose.model('Review', ReviewSchema);
